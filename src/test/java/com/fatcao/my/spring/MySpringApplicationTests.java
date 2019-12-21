3e64941bc26312851d6f185322f86c213c9b1631
fatcao <1176281215@qq.com>
2019-12-21 22:27:58 +0800

    提交项目

---
+++ src/test/java/com/fatcao/my/spring/MySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatcao.my.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
